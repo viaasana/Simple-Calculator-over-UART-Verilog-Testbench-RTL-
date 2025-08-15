550a32f474adabfe9e468f45c93cc09f320cdc5b
viasana <107901948+viaasana@users.noreply.github.com>
2025-08-15 11:48:07 +0700

    Update tb_simple_caculator.v to test pipline module

--- tb_simple_caculator.v
+++ tb_simple_caculator.v
@@ -8,36 +8,60 @@ module tb_simple_caculator;
     parameter BIT_DURATION = 104.167; // 1/(baud rate) 
 
     reg [7:0] tb_data;
-    wire [7:0] tb_result;
+    wire [7:0] tb_result_pipeline, tb_result_nonpipeline;
     reg TX_START, E, CLK;
-    wire TX_LINE, RX_LINE, error;
+    wire TX_LINE, RX_LINE_for_pipe_line, RX_LINE_for_nonpipeline;
     reg RST = 0;
 
-
-
+    // UART for pipeline calculator
     uart #(
         .CLK_FREQ(CLK_FREQ),
         .BAUD(BAUD)
-    ) user (
+    ) user_use_pipeline (
         .CLK(CLK),
         .RST(RST),
-        .RX(RX_LINE),
+        .RX(RX_LINE_for_pipe_line),
         .TX(TX_LINE),
         .TX_START(TX_START),
         .TX_DATA(tb_data),
-        .RX_DATA(tb_result),
+        .RX_DATA(tb_result_pipeline),
         .value_intr()
     );
 
-    simple_caculator uut(
+    // UART for non-pipeline calculator
+    uart #(
+        .CLK_FREQ(CLK_FREQ),
+        .BAUD(BAUD)
+    ) user_use_nonpipeline (
+        .CLK(CLK),
+        .RST(RST),
+        .RX(RX_LINE_for_nonpipeline),
+        .TX(TX_LINE),
+        .TX_START(TX_START),
+        .TX_DATA(tb_data),
+        .RX_DATA(tb_result_nonpipeline),
+        .value_intr()
+    );
+
+    // Pipeline calculator instance
+    simple_caculator_v2 pipelineCalculator(
         .E(E),
         .CLK(CLK),
         .RX(TX_LINE),
         .RST(RST),
-        .TX(RX_LINE)
+        .TX(RX_LINE_for_pipe_line)
     );
 
-    // Task to send data
+    // Non-pipeline calculator instance
+    simple_caculator nonpipelineCalculator(
+        .E(E),
+        .CLK(CLK),
+        .RX(TX_LINE),
+        .RST(RST),
+        .TX(RX_LINE_for_nonpipeline)
+    );
+
+    // Task to send UART data
     task send;
         input [7:0] data;
         begin
@@ -50,10 +74,12 @@ module tb_simple_caculator;
 
     always #(CLK_PERIOD/2) CLK = ~CLK;
 
+
     initial begin
-        $monitor("%0t Result send back: %d", $time, tb_result);
+        $monitor("%0t ns | Result sent from none-pipline: %d", $time, tb_result_nonpipeline);
         CLK = 0;
         RST = 0;
+        TX_START = 0;
         #10;
         RST = 1;
         E = 1;
@@ -61,9 +87,9 @@ module tb_simple_caculator;
         send(8'd5);
         #(BIT_DURATION*11);
         send("+");
-        #(BIT_DURATION*11);
+        #(BIT_DURATION*15);
         send(8'd10);
-        #(BIT_DURATION*11);
+        #(BIT_DURATION*16);
         send(8'd30);
         #(BIT_DURATION*11);
         send("-");
@@ -85,13 +111,159 @@ module tb_simple_caculator;
         send(8'd15);
         #(BIT_DURATION*11);
         send("/");
-        #(BIT_DURATION*11);
+        #(BIT_DURATION*20);
         send(8'd0);
-        
-
+        #(BIT_DURATION*11);
+        send(8'd5);
+        #(BIT_DURATION*11);
+        send("+");
+        #(BIT_DURATION*15);
+        send(8'd10);
+        #(BIT_DURATION*16);
+        send(8'd30);
+        #(BIT_DURATION*11);
+        send("-");
+        #(BIT_DURATION*11);
+        send(8'd20);
+        #(BIT_DURATION*11);
+        send(8'd15);
+        #(BIT_DURATION*11);
+        send("x");
+        #(BIT_DURATION*11);
+        send(8'd10);
+        #(BIT_DURATION*11);
+        send(8'd50);
+        #(BIT_DURATION*11);
+        send("/");
+        #(BIT_DURATION*11);
+        send(8'd5);
+        #(BIT_DURATION*11);
+        send(8'd15);
+        #(BIT_DURATION*11);
+        send("/");
+        #(BIT_DURATION*20);
+        send(8'd0);
+        #(BIT_DURATION*11);
+        send(8'd5);
+        #(BIT_DURATION*11);
+        send("+");
+        #(BIT_DURATION*15);
+        send(8'd10);
+        #(BIT_DURATION*16);
+        send(8'd30);
+        #(BIT_DURATION*11);
+        send("-");
+        #(BIT_DURATION*11);
+        send(8'd20);
+        #(BIT_DURATION*11);
+        send(8'd15);
+        #(BIT_DURATION*11);
+        send("x");
+        #(BIT_DURATION*11);
+        send(8'd10);
+        #(BIT_DURATION*11);
+        send(8'd50);
+        #(BIT_DURATION*11);
+        send("/");
+        #(BIT_DURATION*11);
+        send(8'd5);
+        #(BIT_DURATION*11);
+        send(8'd15);
+        #(BIT_DURATION*11);
+        send("/");
+        #(BIT_DURATION*20);
+        send(8'd0);
+        #(BIT_DURATION*11);
+        send(8'd5);
+        #(BIT_DURATION*11);
+        send("+");
+        #(BIT_DURATION*15);
+        send(8'd10);
+        #(BIT_DURATION*16);
+        send(8'd30);
+        #(BIT_DURATION*11);
+        send("-");
+        #(BIT_DURATION*11);
+        send(8'd20);
+        #(BIT_DURATION*11);
+        send(8'd15);
+        #(BIT_DURATION*11);
+        send("x");
+        #(BIT_DURATION*11);
+        send(8'd10);
+        #(BIT_DURATION*11);
+        send(8'd50);
+        #(BIT_DURATION*11);
+        send("/");
+        #(BIT_DURATION*11);
+        send(8'd5);
+        #(BIT_DURATION*11);
+        send(8'd15);
+        #(BIT_DURATION*11);
+        send("/");
+        #(BIT_DURATION*20);
+        send(8'd0);
+        #(BIT_DURATION*11);
+        send(8'd5);
+        #(BIT_DURATION*11);
+        send("+");
+        #(BIT_DURATION*15);
+        send(8'd10);
+        #(BIT_DURATION*16);
+        send(8'd30);
+        #(BIT_DURATION*11);
+        send("-");
+        #(BIT_DURATION*11);
+        send(8'd20);
+        #(BIT_DURATION*11);
+        send(8'd15);
+        #(BIT_DURATION*11);
+        send("x");
+        #(BIT_DURATION*11);
+        send(8'd10);
+        #(BIT_DURATION*11);
+        send(8'd50);
+        #(BIT_DURATION*11);
+        send("/");
+        #(BIT_DURATION*11);
+        send(8'd5);
+        #(BIT_DURATION*11);
+        send(8'd15);
+        #(BIT_DURATION*11);
+        send("/");
+        #(BIT_DURATION*20);
+        send(8'd0);
+        #(BIT_DURATION*11);
+        send(8'd5);
+        #(BIT_DURATION*11);
+        send("+");
+        #(BIT_DURATION*15);
+        send(8'd10);
+        #(BIT_DURATION*16);
+        send(8'd30);
+        #(BIT_DURATION*11);
+        send("-");
+        #(BIT_DURATION*11);
+        send(8'd20);
+        #(BIT_DURATION*11);
+        send(8'd15);
+        #(BIT_DURATION*11);
+        send("x");
+        #(BIT_DURATION*11);
+        send(8'd10);
+        #(BIT_DURATION*11);
+        send(8'd50);
+        #(BIT_DURATION*11);
+        send("/");
+        #(BIT_DURATION*11);
+        send(8'd5);
+        #(BIT_DURATION*11);
+        send(8'd15);
+        #(BIT_DURATION*11);
+        send("/");
+        #(BIT_DURATION*20);
+        send(8'd0);
+        #(BIT_DURATION*11);
     end
-
-
-
 
 endmodule
